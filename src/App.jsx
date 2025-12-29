a3904fcb8d740aa694fcbece6ce2825601329bee
Prajwal2246 <janbandhuprajwal28@gmail.com>
2025-12-29 18:30:06 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import RegisterAsDonar from "./pages/registerAsDonar";
+import RegisterAsDonar from "./pages/RegisterAsDonar";
 import Donorlist from "./pages/Donorlist";
 import MyRequests from "./pages/MyRequests";
 import ReceivedRequests from "./pages/ReceivedRequests";
