83d10c46a186b9cf100829cbcefcd2a91b0213fb
Prajwal2246 <janbandhuprajwal28@gmail.com>
2025-12-29 18:29:08 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Register from "./pages/Register";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
 import RegisterAsDonar from "./pages/registerAsDonar";
 import Donorlist from "./pages/Donorlist";
 import MyRequests from "./pages/MyRequests";
-import  ReceivedRequests  from "./pages/ReceivedRequests"
+import ReceivedRequests from "./pages/ReceivedRequests";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/register-as-donar" element={<RegisterAsDonar/>} />
-        <Route path="/donors" element={<Donorlist/>} />
-        <Route path="/requests" element={<MyRequests/>} />
-        <Route path="/receivedRequest" element={<ReceivedRequests/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/register-as-donar" element={<RegisterAsDonar />} />
+        <Route path="/donors" element={<Donorlist />} />
+        <Route path="/requests" element={<MyRequests />} />
+        <Route path="/receivedRequest" element={<ReceivedRequests />} />
       </Routes>
     </BrowserRouter>
   );
